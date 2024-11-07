81a0b983f74291f826b2278906f287e5bb68b402
Caio Gustavo Guimarães <caiogcg.mobilidade@gmail.com>
2024-11-07 13:55:50 -0300

    Add files via upload

--- R/table_perc.R
+++ R/table_perc.R
@@ -1,11 +1,18 @@
 
 
-table_perc <- function(x){
-  x <- table(x) %>% sort()
-  nm <- names(x)
-  x <- x/sum(x)
-  x <- percent(as.numeric(x),.1)
-  names(x) <- nm
+table_perc <- function(x,chr =  TRUE){
+  checkmate::assert_logical(chr)
+  if(chr){
+    x <- table(x) %>% sort()
+    nm <- names(x)
+    x <- x/sum(x)
+    x <- percent(as.numeric(x),.1)
+    names(x) <- nm
+  }else{
+    x <- table(x) %>% sort()
+    x <- x/sum(x)
+  }
+  
   return(x)
 }
 
